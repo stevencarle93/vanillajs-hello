4cef654758ef8cae3b1b5ddad05c1ab8b189e537
stevencarle93 <stevencarle@hotmail.com>
2022-07-11 02:55:26 +0000

    Excuse Generator Finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+function InitialLoad() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  console.log("Archivo JS conectado con el HTML");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let arrExcuse = [who, action, what, when];
+  let logs = "<ul>";
+  console.log("Arreglos sin mapear: " + JSON.stringify(arrExcuse));
+  arrExcuse = arrExcuse.map(arreglo => {
+    //Abbreviated version of function (arreglo){ //
+    let num = Math.floor(Math.random() * (arreglo.length - 1));
+    logs = logs + "<li>" + num + ":" + arreglo[num] + "</li>";
+    return arreglo[num];
+  });
+  logs = logs + "</ul>";
+  document.getElementById("logs").innerHTML = logs;
+  console.log("Elementos aleatorios por arreglo: " + JSON.stringify(arrExcuse));
+  let excuse = arrExcuse.join(" "); //Build the excuse
+  let excuseDOM = document.getElementById("excusa");
+  excuseDOM.innerText = excuse;
+}
+
+window.addEventListener("DOMContentLoaded", InitialLoad);
